80037c86adf7597eae9b49f3d8405d5b5ee90c5b
nejton <christiannordahl@hotmail.com>
2016-12-14 13:19:08 +0100

    Donewith first

---
+++ nejton/06/6.py
@@ -0,0 +1,16 @@
+dicts = [{} for x in range(0,8)]
+with open("data.txt", "r") as f:
+    for line in f:
+        i = 0
+        for c in list(line.rstrip()):
+            if c not in dicts[i]:
+                dicts[i][c] = 0
+            dicts[i][c] += 1
+            i+=1
+
+    message = ""
+    for i in range(0,8):
+        values = list(dicts[i].values())
+        keys = list(dicts[i].keys())
+        message += str(keys[values.index(max(values))])
+    print message
