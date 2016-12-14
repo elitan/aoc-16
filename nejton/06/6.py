1b55d4b55a858b781d377c2c4b658bbd1d7d9950
nejton <christiannordahl@hotmail.com>
2016-12-14 13:21:47 +0100

    done with 2

--- nejton/06/6.py
+++ nejton/06/6.py
@@ -13,4 +13,11 @@ with open("data.txt", "r") as f:
         values = list(dicts[i].values())
         keys = list(dicts[i].keys())
         message += str(keys[values.index(max(values))])
-    print message
+    print "1:", message
+
+    message = ""
+    for i in range(0,8):
+        values = list(dicts[i].values())
+        keys = list(dicts[i].keys())
+        message += str(keys[values.index(min(values))])
+    print "2:", message
