36ac3546a202e9d27a2f12c915d15f0b1a79b532
Thomas Sievert <thomas.sievert@bth.se>
2016-12-21 14:09:55 +0100

    Small fix
    Had some debug stuff in day 15 left

--- thomasjevskij/15/python/15.py
+++ thomasjevskij/15/python/15.py
@@ -4,8 +4,6 @@ t = time.process_time()
 
 discs = []
 
-lines = [ 'Disc #1 has 5 positions; at time=0, it is at position 4.',
-          'Disc #2 has 2 positions; at time=0, it is at position 1.' ]
 with open('input.txt') as f:
     lines = f.readlines()
 
